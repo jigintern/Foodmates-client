818fdfdd2d0de341b8c03b1ca35fc17f74d6e0cb
watano1168 <watano1168@gmail.com>
2019-08-26 09:39:18 +0900

    [ADD] ヘッダーコンポーネントの雛型を作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,54 @@
 <template>
-  <HelloWorld />
+  <div>
+    <v-container>
+      <v-layout wrap>
+        <v-flex xs6 md3 v-for="(p, index) in posts" :key="`${index}`">
+          <v-card color="#aaaaaa" height=280 class="ma-2 pa-2">
+            <v-img :src="`/img/test${index}.jpg`" />
+            <h1>{{ p.title }}</h1>
+            <p>{{ p.content }}</p>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
 export default {
-  components: {
-    HelloWorld,
-  },
-};
+  data () {
+    return {
+      posts: [
+        {
+          title: "タイトル1",
+          content: "文章1"
+        },
+        {
+          title: "タイトル2",
+          content: "文章2"
+        },
+        {
+          title: "タイトル3",
+          content: "文章3"
+        },
+        {
+          title: "タイトル3",
+          content: "文章3"
+        },
+        {
+          title: "タイトル3",
+          content: "文章3"
+        },
+        {
+          title: "タイトル3",
+          content: "文章3"
+        },
+        {
+          title: "タイトル3",
+          content: "文章3"
+        }
+      ]
+    }
+  }
+}
 </script>
