542f0dea244e8ccad4c9a4641a216652fdf22732
umbroom <omori.fuki@gmail.com>
2019-08-27 11:49:06 +0900

    [ADD]HeaderSearchWindow

---
+++ src/components/TheHeader/TheHeaderSearchWindow/TheHeaderSearchWindow.vue
@@ -0,0 +1,13 @@
+<template>
+   <v-form>
+      <v-text-field
+        label="検索"
+        outlined
+    ></v-text-field>
+  </v-form>
+</template>
+
+<style lang="stylus" scoped>
+  .v-messages theme--light
+    display:none
+</style>
