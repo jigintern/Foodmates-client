e13b51ebbb1f7830e0f64bdeabd67dfca80bd9b4
umbroom <omori.fuki@gmail.com>
2019-08-29 17:24:00 +0900

    fix: header-search-form-color

--- src/components/TheHeader/TheHeaderSearchWindow/TheHeaderSearchWindow.vue
+++ src/components/TheHeader/TheHeaderSearchWindow/TheHeaderSearchWindow.vue
@@ -3,3 +3,9 @@
      <input v-model="message" placeholder="Search" class="white orange--text px-4" style="width:640px;height:32px;font-size:16px;border-radius:16px;outline:0;">
   </v-form>
 </template>
+
+<style scoped>
+input::placeholder{
+  color: orange;
+}
+</style>
