9f6ce579b76ea48309ee4bb63cb831757970682c
watano1168 <watano1168@gmail.com>
2019-09-03 16:42:08 +0900

    [CHANGE] タイムラインの見た目変更

--- src/components/ThePostModal/ThePostModal.vue
+++ src/components/ThePostModal/ThePostModal.vue
@@ -1,15 +1,52 @@
 <template>
 <div>
-  <v-card class="pa-4" light width=360>
-    <search-window />
-    <div class="d-flex align-center">
-      <input id="photo" type="file" v-on:change="uploadPhoto" style="display: none;"/>
+  <v-card class="pa-8" light>
 
-      <v-btn icon class="mx-2 orange--text"><v-icon>mdi-message-text</v-icon></v-btn>
-      <v-btn icon class="orange--text"><label for="photo"><v-icon>mdi-message-image</v-icon></label></v-btn>
-      <v-btn color="orange" type="submit" @click="onSubmit">投稿</v-btn>
-      <v-btn outlined class="mx-2 orange--text" @click="onCancel">キャンセル</v-btn>
-    </div>
+    <v-col>
+      <v-row>
+        <v-card v-if="selectedDish" class="d-flex align-center px-6 py-1 mb-2" @click="selectedDish=0" style="cursor:pointer;">
+          <div class="suggest-info">
+            <h2 class="body-1 suggest-info">{{ selectedDish.dish_name }}</h2>
+            <h5 class="caption grey--text suggest-info">{{ selectedDish.store_name }} , 茨城県, ひたちなか市</h5>
+          </div>
+        </v-card>
+      </v-row>
+
+      <v-row justify="center">
+        <v-col cols=12>
+          <search-window @select_dish="selectDish" />
+        </v-col>
+      </v-row>
+
+      <input type="file" @change="onImageChange" style="display: none;" ref="image">
+
+      <v-row justify="center">
+        <img :src="uploadedImageForView" style="max-height:200px; max-width=200px;" class="pa-3" />
+      </v-row>
+
+      <v-row justify="center">
+        <v-textarea v-model="comment" outlined label="コメント" />
+      </v-row>
+
+      <v-row justify="center">
+        <v-col v-if="!uploadedImageForView" cols="2">
+          <v-btn icon @click="pickImage">
+            <v-icon color="orange">mdi-camera-enhance</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col v-else cols="2">
+          <v-btn icon @click="clearImage">
+            <v-icon color="orange">close</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col cols="4" class="px-1">
+          <v-btn outlined class="orange--text" @click="onCancel">キャンセル</v-btn>
+        </v-col>
+        <v-col cols="4" class="px-1">
+          <v-btn color="orange" type="submit" @click="onSubmit" block>投稿</v-btn>
+        </v-col>
+      </v-row>
+    </v-col>
   </v-card>
 </div>
 </template>
@@ -17,76 +54,138 @@
 <script>
 import SearchWindow from './TheSearchWindow/TheSearchWindow'
 
+const fileExtensions = ['jpg', 'jpeg', 'png', 'bmp', 'gif']
+const fileMaxSize = 1e+7
+
 export default {
   components: {
     SearchWindow,
   },
   data () {
     return {
-      photoUrl: '',
+      uploadedImageForView: null,
+      postImage: null,
       comment: '',
-      dishId: 0,
+      selectedDish: null,
       headers: {
         'Content-Type':'application/x-www-form-urlencoded',
       }
     }
   },
+
   methods: {
-    uploadPhoto (event) {
-      console.log("[ThePostModal.vue] updatePhoto()")
-      console.log("this.photoUrl" + this.photoUrl)
-      let files = event.target.files || event.dataTransfer.files
-      let file = files[0]
+    async addPost () {
+      await this.myServer.post(
+        '/upload/',
+        this.postImage,
+        { headers: { 'content-type': 'multipart/form-data' }}
+      ).then(res => {
+        console.log('画像を投稿しました: ' + res.data)
+      }).catch(function (error) {
+        console.log(error)
+      })
+    },
+
+    pickImage () {
+      this.$refs.image.click()
+    },
+
+    clearImage() {
+      this.postImage = null
+      this.uploadedImageForView = null
+    },
+
+    createImage (file) {
       let reader = new FileReader()
       reader.onload = (e) => {
-        this.photoUrl = e.target.result
+        var uploadedImage = e.target.result
+        this.uploadedImageForView = uploadedImage
       }
       reader.readAsDataURL(file)
     },
 
+    onImageChange (e) {
+      console.log("[ThePostModal.vue] onFileChange()")
+      let files = e.target.files || e.dataTransfer.files
+
+      // ファイルタイプのチェック
+      var fileTypeCheck = false
+      var myFileType = files[0].type
+      for (var i = 0; i < fileExtensions.length; i++) {
+        var fileExtension = fileExtensions[i]
+        if (myFileType.indexOf(fileExtension) > -1) {
+          fileTypeCheck = true
+          break
+        }
+      }
+      if (!fileTypeCheck) {
+        console.log('アップロードできるファイルは画像のみです。')
+        return
+      }
+
+      // ファイルサイズのチェック
+      if (files[0].size > fileMaxSize) {
+        console.log('ファイルサイズが大きすぎます。')
+        return
+      }
+
+      this.postImage = files[0]
+      this.createImage(files[0])
+    },
+
     async onSubmit() {
       console.log("[ThePostModal.vue] onSubmit()")
-      const content = {
-        photoUrl: this.photoUrl,
-        comment: this.comment,
-        dishId: this.dishId
+      
+      if (!this.uploadedImageForView || !this.postImage) {
+        this.addPost()
       }
-      console.log("this.content" + content)
+
+      const content = {
+        "UserId": 1,
+        "DishId": this.selectDish.Id,
+        "Comment": this.comment
+      }
+      console.log('send content: ' + content)
       try {
-        /*
-        await this.myServer.post(
-          '/posts/create',
-          {
-            "user_id": 1,
-            "dish_id": this.dishID,
-            "comment": this.comment,
-            "image_address": this.photoUrl
-          },
-        )
-        */
         await this.myServer.post(
           '/posts/create/',
-          {
-            "user_id": 1,
-            "dish_id": this.dishId,
-            "comment": this.comment,
-            "image_address": this.photoUrl
-          },
+          content,
           { headers: this.headers }
         )
       } catch (e) {
         console.log('post error')
       }
       this.$emit('on_submit')
-      this.photoUrl = '',
-      this.comment = '',
+      this.postImage = null
+      this.uploadedImageForView = null
+      this.comment = ''
       this.dishId = 0
     },
 
     async onCancel() {
       console.log("[ThePostModal.vue] onCancel()")
       this.$emit('on_cancel')
+    },
+
+    selectDish(dish) {
+      this.selectedDish = dish
     }
   }
 }
 </script>
+
+<style scoped>
+.suggest-info {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
+.row {
+  padding: 0;
+}
+
+.col {
+  padding: 0;
+}
+</style>
